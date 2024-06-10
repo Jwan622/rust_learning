809d97fc0cb7a881e114e61dc5b88280495a6ab2
Jeffrey <jwan622@gmail.com>
2024-06-11 00:56:31 +0200

    finishing lifetimes

---
+++ lifetimes/target/debug/deps/lifetimes-1d83d375f0b976d1.d
@@ -0,0 +1,7 @@
+/Users/jwan/Desktop/programming/rust_learning/lifetimes/target/debug/deps/liblifetimes-1d83d375f0b976d1.rmeta: src/lib.rs
+
+/Users/jwan/Desktop/programming/rust_learning/lifetimes/target/debug/deps/liblifetimes-1d83d375f0b976d1.rlib: src/lib.rs
+
+/Users/jwan/Desktop/programming/rust_learning/lifetimes/target/debug/deps/lifetimes-1d83d375f0b976d1.d: src/lib.rs
+
+src/lib.rs:
